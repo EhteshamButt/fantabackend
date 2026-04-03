0c675829d4bb7b2d9caccec7bcf72410b954b66e
Ehtesham Ali <buttehtesaham86@gmail.com>
2026-04-03 09:24:10 -0800

    sa

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -13,14 +13,18 @@ export class RegisterDto {
   @IsNotEmpty()
   email: string;
 
+  // @IsString()
+  // @MinLength(8)
+  // @MaxLength(64)
+  // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/, {
+  //   message:
+  //     'Password must contain uppercase, lowercase, number, and special character',
+  // })
+  // password: string;
   @IsString()
-  @MinLength(8)
-  @MaxLength(64)
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/, {
-    message:
-      'Password must contain uppercase, lowercase, number, and special character',
-  })
-  password: string;
+@MinLength(6)
+@MaxLength(64)
+password: string;
 
   @IsString()
   @IsNotEmpty()
