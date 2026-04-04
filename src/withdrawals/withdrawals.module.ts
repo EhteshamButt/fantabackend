ed21d78feb68022511bd5c3e36396b7322366968
Ehtesham Ali <buttehtesaham86@gmail.com>
2026-04-04 00:59:13 -0800

    a

--- src/withdrawals/withdrawals.module.ts
+++ src/withdrawals/withdrawals.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { WithdrawalsController } from './withdrawals.controller';
 import { WithdrawalsService } from './withdrawals.service';
 import { Withdrawal } from './withdrawal.entity';
+import { User } from '../users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Withdrawal])],
+  imports: [TypeOrmModule.forFeature([Withdrawal, User])],
   controllers: [WithdrawalsController],
   providers: [WithdrawalsService],
   exports: [WithdrawalsService],
